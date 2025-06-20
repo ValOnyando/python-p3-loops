db91541c5590c15193b1574a4ee40a4341a57d22
Vallary Awuor Onyando <valonyando@gmail.com>
2025-06-20 17:57:49 +0300

    Tasks done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+def square_integers(int_list):  
+    squared = []
+    for inte in int_list:
+        square = inte ** 2
+        squared.append(square)
+    return squared
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if (i % 3) == 0 and (i % 5) == 0:
+            print("FizzBuzz")
+        elif (i % 3) == 0:
+            print("Fizz")
+        elif (i % 5) == 0:
+            print("Buzz")
+        else:
+            print(i)
